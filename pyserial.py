4aed3f52b6bb2f2da5072c9767f0654c003eb194
irreality4free <irreality4free@gmail.com>
2018-03-15 22:35:53 +0300

    bytearray from hex

--- pyserial.py
+++ pyserial.py
@@ -1,10 +1,6 @@
 from time import sleep
-import serial
-#ser = serial.Serial('COM6', 9600) # Establish the connection on a specific port
-counter = 32 # Below # 32 everything in ASCII is gibberish
-p = "FF AE 00 0C AE 11 34 56 00 00"
-# def make_packet():
-#     pack = ""
+
+
 from time import sleep
 import serial
 ser = serial.Serial('COM6', 9600) # Establish the connection on a specific port
@@ -14,26 +10,29 @@ counter = 32 # Below # 32 everything in ASCII is gibberish
 p = "FF AE      00 0C   00      00      01  01  10 12       00 00"
 p1= "FF AE      00 0C   00      00      03  01  10          00 00"
 
-#
-# def make_packet():
-#     pack = ""
-#     pack += chr(0xff)+chr(0xae)
-#     pack +=
 
 def pack2str(p):
-    return " ".join(["%x"%c for c in p])
+    return " ".join(["%02x"%c for c in p])
+
 
 def str2pack(s):
-    return [chr(eval("0x%s"%l)) for l in s.split()]
-ser.write(p.encode('utf-8'))#
+    # return bytes("".join([chr(eval("0x%s"%l)) for l in s.split()]),'utf-8')
+    # return [chr(eval("0x%s"%l)) for l in s.split()]
+    #return p1.split()
+    return bytearray.fromhex(s)
+
+print(str2pack(p1))
+print(pack2str(str2pack(p1)))
+ser.write(str2pack(p))
+ser.write(str2pack(p))
+# for x in str2pack(p1):
+#     ser.write(str2pack(p))
+# ser.write(p.encode('utf-8'))
+#
 while True:
 
-     counter +=1
+
       # Convert the decimal number to ASCII then send it to the Arduino
      # ser.write(counter)
-     print(pack2str(ser.readline())) # Read the newest output from the Arduino
-     sleep(1) # Delay for one tenth of a second
-     if counter == 255:
-        counter = 32
-
-# print(str(0x10))
+     print(ser.readline()) # Read the newest output from the Arduino
+     sleep(0.1) # Delay for one tenth of a second
